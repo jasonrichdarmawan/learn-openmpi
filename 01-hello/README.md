b9e2eefd44645af365c12f9a3a824595084dcd21
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2025-01-03 19:42:42 +0800

    first commit

---
+++ 01-hello/README.md
@@ -0,0 +1,13 @@
+# How to use?
+
+## How to build?
+
+```
+mpicc ./main.c -o main
+```
+
+## How to run?
+
+```
+mpirun ./main
+```
---
+++ 01-hello/main.c
@@ -0,0 +1,17 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    MPI_Init(NULL, NULL);
+    int rank;
+    int num_procs;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    // This function indicates the number of processes involved in a communicator. 
+    // For MPI_COMM_WORLD, it indicates the total number of processes available
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+
+    // Do many things, all at once
+    printf("Hello: rank %d, num_procs: %d\n", rank, num_procs);
+    
+    MPI_Finalize();
+}
